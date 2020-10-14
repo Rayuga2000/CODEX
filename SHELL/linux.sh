a7c4474743f5a1a8179eb7601c9e1751538d9d6f
Rayuga2000 <tsarkar431@gmail.com>
2020-10-14 16:09:13 +0530

    updated Naruto page

--- SHELL/linux.sh
+++ SHELL/linux.sh
@@ -9,20 +9,16 @@ sudo apt-get update && sudo apt-get install sublime-text;
 
 sudo apt-add-repository -y ppa:teejee2008/ppa;sudo apt-get update;sudo apt-get install timeshift;
 
+sudo apt-get install gnome-tweaks -y;
 sudo apt-get install code --classic;
-
-sudo apt install software-properties-common;
-sudo add-apt-repository ppa:philip.scott/elementary-tweaks;
-sudo apt install elementary-tweaks;
+sudo apt-get update;
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
 sudo apt install ./google-chrome-stable_current_amd64.deb -y;
 
 sudo apt-get install pulseaudio pavucontrol -y;
-sudo apt-get install libreoffice -y;
-sudo apt-get install gimp -y;
 sudo apt-get install vlc -y;
 
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list;
 sudo apt-get update && sudo apt-get install spotify-client;
