d6d9158f1a3d3415c7b36f3f3bb12896d48a2e2a
Rayuga2000 <tsarkar431@gmail.com>
2021-05-10 22:18:03 +0530

    C++

---
+++ C++/Write a program that prints out the letters ‘a’....’z’ and the digits ‘0’....’9’ and their ASCII values using recursion./main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void number(char c)
+{
+	if(c<='9')
+	{
+		cout <<c <<" = " <<(int)c <<endl;
+		number(c+1);
+	}
+}
+void alphabet(char c)
+{
+	if(c<='z')
+	{
+		cout <<c <<" = " <<(int)c <<endl;
+		alphabet(c+1);
+	}
+}
+
+int main()
+{
+	cout <<"Numbers" <<endl;
+	number('0');
+	cout <<"Alphabets:" <<endl;
+	alphabet('a');
+
+	return 0;
+}
