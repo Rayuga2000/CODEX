876c6011d4cda1a3818cfa5e0bea5c212195ef29
Rayuga2000 <tsarkar431@gmail.com>
2021-05-15 15:15:06 +0530

    Assignment

---
+++ C++/Assignment/Define a macro Cube1(a) in C++ and call it from main( ) with long and integer argument./main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+#define Cube1(a) (6*a*a)
+
+int main()
+{
+    long x=50000000;
+    int y=5000;
+
+    cout<<"Area of Cube 1 is=> "<<Cube1(x)<<endl;
+    cout<<"Area of Cube 2 is=> "<<Cube1(y)<<endl;
+
+    return 0;
+}
---
+++ C++/Assignment/Write a C++ program to find the largest of three numbers using inline function./main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+inline int max(int x,int y,int z)
+{
+    return (x>y)? ((x>z)? x:z):((y>z)? y:z);
+}
+int main()
+{
+    int x,y,z;
+
+    cout<<"Enter Three Numbers: ";
+    cin>>x>>y>>z;
+
+    cout<<"The Largest Number is=> "<<max(x,y,z)<<endl;
+
+    return 0;
+}
