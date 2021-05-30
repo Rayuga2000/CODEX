9c1a1230adf4deebe3c77f9f87f6552623425494
Rayuga2000 <tsarkar431@gmail.com>
2021-05-31 04:52:59 +0530

    C++ Day 2

--- C++/Class/Day 2/Q2/main.cpp
+++ C++/Class/Day 2/Q2/main.cpp
@@ -1,28 +1,37 @@
-/*Write
-a function that counts the number of occurrences of a pair of letters in a
-string and another that does the same in a zero terminated array of char (a C
-style string). For example, the pair "ab" appears twice in "ax
-ab aa cb ax ab ba".*/
+/*Write a function that counts the number of occurrences of a pair of letters in a
+string and another that does the same in a zero terminated array of char 
+(a C style string).For example, the pair "ab" appears twice in "ax ab aa cb ax ab ba".*/
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main()
 {
-    string str;
-    char x;
+    string str,x;
+    int y=-1,occur=0;
 
     cout<<"Enter the String: ";
     getline(cin,str);
 
-    cout<<"Enter the letter to search: ";
+    cout<<"Enter the pair to search: ";
     cin>>x;
 
-    string::iterator itr;
-
-    for(itr=str.begin();itr!=str.end();itr++)
+    while(1)
     {
-        cout<<*itr;
+        y=str.find(x,y+1);
+
+        if(y!=-1)
+        {
+            occur++;
+        }
+        else
+        {
+            break;
+        }
     }
+
+    cout<<x<<" occured "<<occur<<" times \n";
+
     return 0;
 }
