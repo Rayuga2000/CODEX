27115ecab2a7e8e56602ecfb065059af9034d5de
rayuga <tsarkar431@gmail.com;git config --global user.name Rayuga2000>
2021-07-25 21:17:48 +0530

    LogIn form

---
+++ Web Designing/Portfolio/JavaScript/script.js
@@ -0,0 +1,8 @@
+function check(){
+    var uname=document.getElementById('uname').value;
+    var paswd=document.getElementById('paswd').value;
+
+    if(uname=='Rayuga' && paswd=='im@2000'){
+        window.open("../HTML/main.html")
+    }
+}
