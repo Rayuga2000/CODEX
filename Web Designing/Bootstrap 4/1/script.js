3c6563f2b803fd2274376d9ec47ecf2a058658a7
Rayuga2000 <tsarkar431@gmail.com>
2021-08-05 15:58:06 +0530

    Bootstrap

--- Web Designing/Bootstrap 4/1/script.js
+++ Web Designing/Bootstrap 4/1/script.js
@@ -1,24 +1,3 @@
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // Get the forms we want to add validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
-
 $(document).ready(function(){
-  $(".custom-file-input").on("change", function() {
-  var fileName = $(this).val().split("\\").pop();
-  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-});
+  $('[data-toggle="tooltip"]').tooltip();
 });
