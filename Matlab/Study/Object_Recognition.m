d6b031d7efd841679b4b957c6a29310e457902e0
Rayuga <tsarkar431@gmail.com>
2022-04-30 22:22:24 +0530

    Shape Recognition

--- Matlab/Study/Object_Recognition.m
+++ Matlab/Study/Object_Recognition.m
@@ -3,9 +3,8 @@ clear all;
 close all;
 
 nnet=alexnet;
-img=imread('y.png');
+img=imread('x.jpg');
 img=imresize(img,[227,227]);
 label=classify(nnet,img);
 imshow(img);
-title=(upper(char(label)));
-fprintf((upper(char(label))));
+title(upper(char(label)));
