d87cad480d054b92184eed4b413e0a7879159a19
Rayuga2000 <tsarkar431@gmail.com>
2022-05-02 09:42:18 +0530

    python

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,16 +1,13 @@
-def isLeap(year):
-    leap=False
+import numpy as np
 
-    if year%100==0:
-        if year%400==0:
-            leap=True
-    elif year%4==0:
-        leap=True
+r,c=map(int,input("Enter the row and col: ").split())
 
-    return leap
+#mat1=np.array([input("Enter elements for 1st Matrix:") for i in range(r*c)],int)
+mat1=np.array([input("Enter elements for 1st Matrix:").split(" ",(r*c)-1)],int)
+mat2=np.array([input("Enter elements for 2nd Matrix:").split(" ",(r*c)-1)],int)
 
-year=int(input("Enter the Year:"))
-if(isLeap(year)):
-    print("The year is a Leap Year")
-else:
-    print("The year is not a Leap Year")
+mat1.shape=(r,c)
+mat2.shape=(r,c)
+
+print("The 1st matrix is:\n",mat1)
+print("The 2nd matrix is:\n",mat2)
