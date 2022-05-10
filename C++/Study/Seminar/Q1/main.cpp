c81f9463b8836420d0282ab5ded199b5eff01595
Rayuga2000 <tsarkar431@gmail.com>
2022-05-10 22:01:41 +0530

    python

---
+++ C++/Study/Seminar/Q1/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int i=0;
+
+    cout<<"Value of i inside loop: ";
+    for(true){
+        cout<<"Hello"<<" ";
+    }
+    cout<<"\nValue of i ouside loop: "<<i<<endl;
+
+    return 0;
+}
