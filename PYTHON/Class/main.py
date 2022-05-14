bd33f261cba2eaf3c272d694e95c28f788517129
Rayuga2000 <tsarkar431@gmail.com>
2022-05-14 21:00:06 +0530

    python

--- PYTHON/Class/main.py
+++ PYTHON/Class/main.py
@@ -1,4 +1,4 @@
-import numpy as np
+"""import numpy as np
 
 r,c=map(int,input("Enter the row and col: ").split())
 
@@ -10,4 +10,16 @@ mat1.shape=(r,c)
 mat2.shape=(r,c)
 
 print("The 1st matrix is:\n",mat1)
-print("The 2nd matrix is:\n",mat2)
+print("The 2nd matrix is:\n",mat2)"""
+
+from matplotlib import pyplot
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+
+filename='pima(copy).csv'
+data=read_csv(filename)
+#data.hist()
+#data.plot(kind='density',subplots=True,layout=(3,3))
+#data.plot(kind='box',subplots=True,layout=(3,3))
+scatter_matrix(data)
+pyplot.show()
