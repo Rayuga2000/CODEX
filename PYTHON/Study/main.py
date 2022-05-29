694397bce73e39240f7f0ca0b19bc97104837007
Rayuga2000 <tsarkar431@gmail.com>
2022-05-29 22:23:52 +0530

    python

--- PYTHON/Study/main.py
+++ PYTHON/Study/main.py
@@ -1,11 +1,17 @@
 from matplotlib import pyplot
+import pandas as pd
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
 
-filename='pima.csv'
-data=read_csv(filename)
-#data.hist()
-#data.plot(kind='density',subplots=True,layout=(3,3))
-#data.plot(kind='box',subplots=True,layout=(3,3))
-scatter_matrix(data)
-pyplot.show()
+filename='../Class/Datasets/Netflix/titles.csv'
+col_subset=['title','type','genres','release_year','imdb_popularity','imdb_score']
+data=read_csv(filename,usecols=col_subset)
+
+Netflix=pd.DataFrame(data,columns=col_subset)
+Netflix=pd.DataFrame(Netflix.sort_values('imdb_popularity',ascending=False).head(50).values,
+columns=['Title','Type','Genres','Release_year','IMDB_Popularity','IMDB_Score'])
+Netflix.to_csv('Netflix.csv')
+
+Netflix['IMDB_Score'][32,40,47]='4.8','4.3','4.2'
+Netflix.to_csv('Netflix.csv')
+print(Netflix)
